02b550a6506ad730d35a89ba70274908e7f469ae
yanagr <yanagrrrrr@gmail.com>
2020-02-23 16:44:36 +0200

    то,що казали на лекції

--- lab1MAPZ/myinter.h
+++ lab1MAPZ/myinter.h
@@ -1,11 +1,34 @@
 #ifndef MYINTER_H
 #define MYINTER_H
+#include <QString>
+#include <QVector>
+#include <QList>
+#include <QMap>
 
-
-class myInter
-{
-public:
-    myInter();
+enum tokenType{
+                CONST,
+                VARIABLE,
+                CONDITIONAL,
+                CYCLE,
+                FUNCTION,
+                OPERATOR,
+                BRAKETS,
+                SEPARATORS
 };
+
+
+class Token{
+public:
+    Token(QString DATA,tokenType TYPE){data = DATA; type = TYPE;}
+    QString data;
+    tokenType type;
+};
+
+
+QList<Token> lexicalAnalysis(QString code){
+    
+    
+}
+
 
 #endif // MYINTER_H
